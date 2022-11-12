43211dc18bd98a0953c6f47563c1e431cd88087c
Juan Carlos Mancera Suárez <115673584+CarlosSuarezCWH@users.noreply.github.com>
2022-11-12 17:03:21 -0600

    Init proyect

---
+++ main.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, render_template, json
+from obtainForm import *
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/roadmap")
+def roadmap():
+    return render_template("roadmap.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.errorhandler(404)
+def notfound(error):
+    return render_template('404.html'), 404
+
+
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        algebra = float(request.values.get("algebra", None))
+        programacion = float(request.values.get("programacion", None))
+        analisis = float(request.values.get("analisis", None))
+        comunicacion = float(request.values.get("comunicacion", None))
+        fisica = float(request.values.get("fisica", None))
+        ingenieria = float(request.values.get("ingenieria", None))
+        password = request.values.get("password", None)
+        quiere_ayudar= str(request.values.get("ayudar", None))
+        puede_ayudar=str(1)
+        socializar = str(1)
+        hablar = str(request.values.get("hablar", None))
+        explicar = float(request.values.get("explicar", None))
+        Skills=Calculate(algebra, programacion, analisis, comunicacion, fisica, ingenieria, hablar, explicar, socializar, puede_ayudar,quiere_ayudar)
+        return str(Skills)
+if __name__== "__main__":
+    app.run(debug=True)
---
+++ obtainForm.py
@@ -0,0 +1,36 @@
+from main import *
+def calificacion_skills(calificacion,hablar,explicar,socializar,puede_ayudar,quiere_ayudar):
+    if hablar == 1: hablar = 0
+    elif hablar == 0: hablar= 1
+    s1=hablar*5+puede_ayudar*15+socializar*10
+    if calificacion < 10 and calificacion >= 8 and explicar == 9:
+        s2=calificacion*7
+    elif calificacion <10 and calificacion >=8 and explicar ==10:
+        s2=calificacion*7+10
+    elif calificacion==10 and explicar >= 8:
+        s2=calificacion*7
+    elif calificacion==10 and explicar < 8:
+        s2=((calificacion*.1*explicar)*7)+10
+    else:
+        s2=(calificacion*(.1*explicar)*7)
+    if s2 > 70:
+        s2=70
+    if s2 >= 63 and s2 < 70 and s1 < 26:
+        skill=(s1+s2+2)*quiere_ayudar
+    elif s2 == 70 and s1 < 26:
+            skill=(s1+s2+5)*quiere_ayudar    
+    else:
+        skill=(s1+s2)*quiere_ayudar
+    if skill > 10: skill = skill - 10
+    return skill
+
+    return skill
+def Calculate(algebra, programacion, analisis, comunicacion, fisica, ingenieria,hablar,explicar,socializar,puede_ayudar,quiere_ayudar):
+    algebra=calificacion_skills(algebra,float(hablar),explicar,float(socializar),float(puede_ayudar),float(quiere_ayudar))
+    programacion=calificacion_skills(programacion,float(hablar),explicar,float(socializar),float(puede_ayudar),float(quiere_ayudar))
+    analisis=calificacion_skills(analisis,float(hablar),explicar,float(socializar),float(puede_ayudar),float(quiere_ayudar))
+    comunicacion=calificacion_skills(comunicacion,float(hablar),explicar,float(socializar),float(puede_ayudar),float(quiere_ayudar))
+    fisica=calificacion_skills(fisica,float(hablar),explicar,float(socializar),float(puede_ayudar),float(quiere_ayudar))
+    ingenieria=calificacion_skills(ingenieria,float(hablar),explicar,float(socializar),float(puede_ayudar),float(quiere_ayudar))
+    return algebra, programacion, analisis, comunicacion, fisica, ingenieria
+

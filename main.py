e2ae69775c9fad49be94199cf35ca1db0d332b2d
daniel-dmal <danielmaciasal1705@gmail.com>
2022-11-30 08:51:38 -0600

    Cambios (listas)

--- main.py
+++ main.py
@@ -31,23 +31,22 @@ def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
     if request.method == 'POST':
-        Name= str(request.values.get("Name", None))
-        ID_student= str(request.values.get("ID_student",None))
-        algebra = float(request.values.get("algebra", None))
-        programacion = float(request.values.get("programacion", None))
-        analisis = float(request.values.get("analisis", None))
-        comunicacion = float(request.values.get("comunicacion", None))
-        fisica = float(request.values.get("fisica", None))
-        ingenieria = float(request.values.get("ingenieria", None))
-        password = request.values.get("password", None)
-        quiere_ayudar= str(request.values.get("ayudar", None))
-        puede_ayudar=str(1)
-        socializar = str(1)
-        hablar = str(request.values.get("hablar", None))
-        explicar = float(request.values.get("explicar", None))
-        Skills=Calculate(algebra, programacion, analisis, comunicacion, fisica, ingenieria, hablar, explicar, socializar, puede_ayudar,quiere_ayudar)
-        insert_crude()
-        return str(Skills)
+        datos=[]
+        datos.append(request.values.get("Name", None))
+        datos.append(request.values.get("ID_student",None))
+        datos.append(request.values.get("algebra", None))
+        datos.append(request.values.get("programacion", None))
+        datos.append(request.values.get("analisis", None))
+        datos.append(request.values.get("comunicacion", None))
+        datos.append(request.values.get("fisica", None))
+        datos.append(request.values.get("ingenieria", None))
+        datos.append(request.values.get("password", None))
+        datos.append(request.values.get("ayudar", None))
+        datos.append(str(1))
+        datos.append(str(1))
+        datos.append(request.values.get("hablar", None))
+        datos.append(request.values.get("explicar", None))
+        return str(datos)
 
 
 if __name__== "__main__":

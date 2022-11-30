91ed6577cb1cd2920bb3c5b035b7dda2d3a127d8
Juan Carlos Mancera Suárez <115673584+CarlosSuarezCWH@users.noreply.github.com>
2022-11-30 08:15:38 -0600

    cambios 30 nov 1

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, render_template
-from insert import *
-from obtainForm import *
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="cwh1.kiubix.biz",
+  user="fpryvvyl_cwg",
+  password="FELDqXHfgTxv",
+  database="fpryvvyl_global"
+
+)
 app = Flask(__name__)
 @app.route("/")
 def home():
@@ -24,6 +31,9 @@ def form():
 def notfound(error):
     return render_template('404.html'), 404
 @app.route('/data/', methods = ['POST', 'GET'])
+
+
+
 def data():
     if request.method == 'GET':
         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
@@ -45,5 +55,22 @@ def data():
         Skills=Calculate(algebra, programacion, analisis, comunicacion, fisica, ingenieria, hablar, explicar, socializar, puede_ayudar,quiere_ayudar)
         insert_crude()
         return str(Skills)
+
+mycursor = mydb.cursor()
+def insert_crude(ID_student,password,Name):
+    ID_student=data(ID_student)
+    maximun_assignments=1
+    busy_mentor=0
+    email=str(ID_student)*'@globaluniversity.edu.mx'
+    sql = "INSERT INTO crude (id_students, email, password, name, maximum_assignments, busy_mentor) VALUES (%s, %s)"
+    val = [
+        (ID_student, email, password, Name, maximun_assignments, busy_mentor)
+    ]
+    mycursor.executemany(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "was inserted.")
+
+
+
 if __name__== "__main__":
     app.run(debug=True)
